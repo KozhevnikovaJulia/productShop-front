bf71d1327f8a15b2c6808cd352c2f42297f06727
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-04-06 22:22:31 +0300

    total count

--- src/ui/components/ShoppingCartItem.js
+++ src/ui/components/ShoppingCartItem.js
@@ -1,39 +1,56 @@
-import React, { useState, useEffect }  from 'react';
-import '../App.css';
-import Button from '@material-ui/core/Button';
-import { useSelector, useDispatch } from 'react-redux';
-import { setTotalPrise, changeTotalPrise } from '../../bll/reducer'
+import React, { useEffect } from "react";
+import "../App.css";
+import Button from "@material-ui/core/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { setCountOfProduct, setTotalPrice } from "../../bll/reducer";
 
+export const ShoppingCartItem = ({ product, count }) => {
+    const dispatch = useDispatch();
+    const shoppingCart = useSelector((state) => state.app.shoppingCart);
+    const sum = product.price * count;
 
-export const ShoppingCartItem = ({product}) => {
-  const dispatch = useDispatch();  
-  const [quantity, setQuantity] = useState(1)
-  const sum = product.prise*quantity
-  const itemsCount = useSelector(state => state.app.itemsCount)
-  const totalPrise = itemsCount*product.prise
-  const increaseOnClick = () => {
-    dispatch(changeTotalPrise (product.prise, "increase"))
-    setQuantity(quantity+1)
-  }
-  const decreaseOnClick = () => {
-    dispatch(changeTotalPrise (product.prise, "decrease"))
-    setQuantity(quantity-1)
-  }
-  useEffect(() => {
-    dispatch(setTotalPrise (totalPrise))
-  }, []);
-  
-  return (
-    <div className="itemShoppingCart">
-      <div><img src={product.img} /></div>
-      <div>{product.title}{product.description}</div>
-      <div>{product.description}</div>
-      <h1>{sum} $</h1>
-      <Button disabled={quantity<2 ? true : false} onClick={decreaseOnClick} color="inherit" type={'submit'} variant={'contained'}>-</Button>
-      <div>{quantity}</div>
-      <Button onClick={increaseOnClick} color="inherit" type={'submit'} variant={'contained'}>+</Button>
-    </div>
+    const increaseOnClick = () => {
+        dispatch(setCountOfProduct(product.id, "increase"));
+    };
+    const decreaseOnClick = () => {
+        dispatch(setCountOfProduct(product.id, "decrease"));
+    };
+    const currentTotalPrice = shoppingCart.reduce((acc, value) => {
+        return acc + value.product.price * value.count;
+    }, 0);
+    useEffect(() => {
+        dispatch(setTotalPrice(currentTotalPrice));
+    }, [count]);
 
-  );
-}
-
+    return (
+        <div className="itemShoppingCart">
+            <div>
+                <img src={product.img} />
+            </div>
+            <div>
+                {product.title}
+                {product.description}
+            </div>
+            <div>{product.description}</div>
+            <h1>{sum} $</h1>
+            <Button
+                disabled={count < 2 ? true : false}
+                onClick={decreaseOnClick}
+                color="inherit"
+                type={"submit"}
+                variant={"contained"}
+            >
+                -
+            </Button>
+            <div>{count}</div>
+            <Button
+                onClick={increaseOnClick}
+                color="inherit"
+                type={"submit"}
+                variant={"contained"}
+            >
+                +
+            </Button>
+        </div>
+    );
+};
--- src/ui/pages/ShoppingCartPage.js
+++ src/ui/pages/ShoppingCartPage.js
@@ -1,41 +1,51 @@
-import React, { useState, useEffect }  from 'react';
-import '../App.css';
-import { useSelector } from 'react-redux';
-import { ShoppingCartItem } from '../components/ShoppingCartItem';
+import React from "react";
+import "../App.css";
+import { useSelector } from "react-redux";
+import { ShoppingCartItem } from "../components/ShoppingCartItem";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import{ OrderForm } from '../../ui/components/OrderForm'
-
+import { OrderForm } from "../../ui/components/OrderForm";
 
 export const ShoppingCartPage = () => {
-  const shoppingCart = useSelector(state => state.app.shoppingCart)
-  const totalPrise = useSelector(state => state.app.totalPrise)
-  return (
-    <div className="shoppingCart">
-      <div className="shoppingListWrapper">
-        <div className="shoppingList">
-          {shoppingCart.length === 0
-            ? <h1>Сart is empty</h1>
-            : shoppingCart.map((product) => {
-              return <Grid item >
-                <Paper elevation={3} style={{
-                  width: "100%", padding: "15px",
-                  margin:"10px",
-                  backgroundSize: "100% auto"
-                }}
-                  key={product.id}>
-                  <ShoppingCartItem product={product} />
-                </Paper>
-              </Grid>
-            })
-          }
+    const shoppingCart = useSelector((state) => state.app.shoppingCart);
+    const totalPrice = useSelector((state) => state.app.totalPrice);
+
+    return (
+        <div className="shoppingCart">
+            <div className="shoppingListWrapper">
+                <div className="shoppingList">
+                    {shoppingCart.length === 0 ? (
+                        <h1>Сart is empty</h1>
+                    ) : (
+                        shoppingCart.map((item) => {
+                            return (
+                                <Grid item>
+                                    <Paper
+                                        elevation={3}
+                                        style={{
+                                            width: "100%",
+                                            padding: "15px",
+                                            margin: "10px",
+                                            backgroundSize: "100% auto",
+                                        }}
+                                        key={item.product.id}
+                                    >
+                                        <ShoppingCartItem
+                                            product={item.product}
+                                            count={item.count}
+                                        />
+                                    </Paper>
+                                </Grid>
+                            );
+                        })
+                    )}
+                </div>
+                <div className="order">
+                    <OrderForm />
+                </div>
+            </div>
+
+            <h1>Total: {totalPrice} </h1>
         </div>
-        <div className="order">
-          <OrderForm/>
-        </div>
-      </div>
-      
-      <h1>Total: {totalPrise} </h1>
-    </div>
-  );
-}
+    );
+};
