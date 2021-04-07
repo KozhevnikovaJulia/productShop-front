11af3875b738fe050f84f669a7d7c488dd223139
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-04-07 21:43:39 +0300

    api

--- src/dal/api.js
+++ src/dal/api.js
@@ -2,10 +2,10 @@ import Axios from "axios";
 
 export const ShopAPI = {
     getProducts() {
-        return Axios.get("https://lit-springs-31711.herokuapp.com/shop");
+        return Axios.get("https://product-shop-back.herokuapp.com/shop");
     },
     createProduct(productTitle, productPrise, productDescription, productImg) {
-        return Axios.post("https://lit-springs-31711.herokuapp.com/shop", {
+        return Axios.post("https://product-shop-back.herokuapp.com/shop", {
             productTitle,
             productPrise,
             productDescription,
