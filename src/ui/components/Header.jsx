63345aa6216179da7c2ec767f10565de6e525189
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-06-08 11:23:57 +0300

    test

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -1,38 +1,32 @@
-import React from "react";
-import "../App.css";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { useSelector, useDispatch } from "react-redux";
-import { Redirect, Link } from "react-router-dom";
+import React from 'react';
+import '../App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
 
 export const Header = () => {
-    const itemsCount = useSelector((state) => state.app.itemsCount);
-    return (
-        <AppBar
-            position="static"
-            style={{ backgroundColor: "rgb(185, 180, 180)" }}
-        >
-            <Toolbar className={"toolBur"}>
-                <div className={"toolBur__iconBlock"}>
-                    <Typography variant="h6" style={{ marginTop: "8px" }}>
-                        <Link to="/">SHOP</Link>
-                    </Typography>
-                </div>
-                <div className="toolBur__cartWrapper">
-                    <Link to="/cart">
-                        <Button
-                            color="inherit"
-                            type={"submit"}
-                            variant={"contained"}
-                        >
-                            Cart
-                        </Button>
-                    </Link>
-                    <div>{itemsCount}</div>
-                </div>
-            </Toolbar>
-        </AppBar>
-    );
+  const itemsCount = useSelector(state => state.app.itemsCount);
+  return (
+    <AppBar position='static' style={{ backgroundColor: 'rgb(185, 180, 180)' }}>
+      <Toolbar className={'toolBur'}>
+        <div className={'toolBur__iconBlock'}>
+          <Typography variant='h6' style={{ marginTop: '8px' }}>
+            <Link to='/'>SHOP</Link>
+          </Typography>
+        </div>
+        <div className='toolBur__cartWrapper'>
+          <Link to='/cart'>
+            <Button color='inherit' type={'submit'} variant={'contained'}>
+              Cart
+            </Button>
+            <Button>New</Button>
+          </Link>
+          <div>{itemsCount}</div>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
